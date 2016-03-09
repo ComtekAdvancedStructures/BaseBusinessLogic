b0fe7df89465d5b8f399747a564b782ecf2ee406
Craig Verburgh <cverburgh@outlook.com>
2016-03-09 10:45:48 -0500

    Initial Commit

---
+++ BaseBusinessLogic/BaseBusinessLogic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.Entity;
+using Comtek.Helpers;
+
+namespace Comtek
+{
+    public abstract class BaseBusinessLogic
+    {
+        public DbContext Context;
+        private readonly string successMessage;
+        private readonly string failMessage;
+
+        protected BaseBusinessLogic(string appName)
+        {
+            successMessage = $"Changes saved to {appName} database";
+            failMessage = $"There was an error saving the changes to the {appName} database.";
+        }  
+
+        public DataOperationResult Save()
+        {
+            var result = new DataOperationResult();
+            try
+            {
+                Context.SaveChanges();
+                result.Success = true;
+                result.Messages.Add(successMessage);
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.Messages.Add(failMessage);
+                result.Messages.Add(ErrorHelper.GetExceptionMessage(ex));
+            }
+            return result;
+        }
+
+        public DataOperationResult SaveAsync()
+        {
+            var result = new DataOperationResult();
+            try
+            {
+                Context.SaveChangesAsync();
+                result.Success = true;
+                result.Messages.Add(successMessage);
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.Messages.Add(failMessage);
+                result.Messages.Add(ErrorHelper.GetExceptionMessage(ex));
+            }
+            return result;
+        }
+    }
+}
